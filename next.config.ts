185f465061287f213a4853b4c8e5e65867657bb0
Douglas Goodwin <dgoodwin@gmail.com>
2025-12-14 09:13:39 -0800

    update after next.js vulnerability

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    cpus: 1, // Reduces build-time memory usage
+    webpackBuildWorker: true,
+  },
 };
 
 export default nextConfig;
